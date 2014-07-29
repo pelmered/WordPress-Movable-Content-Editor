f998f0bd19ed3cc68857c06d140456246495421b
Peter <peter@elmered.com>
2014-07-29 13:08:17 +0200

    Bugfix. Version bump

--- assets/admin-options.js
+++ assets/admin-options.js
@@ -6,9 +6,6 @@
         $('#movable-content-editor-specify-post-types input:checkbox').change( function() {
             
             var $this = $(this);
-            console.log($(this));
-            
-            alert($this[0].value);
             
             $('#movable-content-editor-specify-titles').append('<input type="text" name="movable-content-editor_options[editor_headers_for_post_types][' + $this[0].value + ']"/>');
             
